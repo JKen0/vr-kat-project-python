032936400b5d244ae765bf8fcaeefaa48dd497ed
Ken <imken0@outlook.com>
2023-06-12 21:46:57 -0400

    started processing standing excel sheets

---
+++ processed-training-data/4-PROCESSED-DATA/python-process-scripts/processStand1.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+FILE_NAME = "STAND1"
+
+PRE_TRAIN_FILE_PATH = "C:\Dev\\vr-kat-project-python-2\processed-training-data\\2-EXTRACT-CSV\PRE-TRAIN-" + FILE_NAME + ".xlsx"
+PROCESS_FILE_PATH = "C:\Dev\\vr-kat-project-python-2\processed-training-data\\4-PROCESSED-DATA\PROCESSED-" + FILE_NAME + ".xlsx"
+
+X_VELOCITY = 0.000
+Z_VELOCITY = 0.000
+
+
+# Read the XLSX file
+df = pd.read_excel(PRE_TRAIN_FILE_PATH)
+
+# Update the columns for every row
+df['X_Vel'] = X_VELOCITY 
+df['Z_Vel'] = Z_VELOCITY
+
+# Save the modified DataFrame to a new XLSX file
+df.to_excel(PROCESS_FILE_PATH, index=False)
