046cb1709f5a2e93d8e695ecf473235a8854acf1
Ken <imken0@outlook.com>
2023-06-12 22:17:26 -0400

    process standing and diagonal and side steps

---
+++ processed-training-data/4-PROCESSED-DATA/python-process-scripts/processRightStep.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+FILE_NAME = "RIGHT-STEPS"
+
+PRE_TRAIN_FILE_PATH = "C:\Dev\\vr-kat-project-python-2\processed-training-data\\2-EXTRACT-CSV\PRE-TRAIN-" + FILE_NAME + ".xlsx"
+PROCESS_FILE_PATH = "C:\Dev\\vr-kat-project-python-2\processed-training-data\\4-PROCESSED-DATA\PROCESSED-" + FILE_NAME + ".xlsx"
+
+X_VELOCITY = 2.000
+Z_VELOCITY = 0.000
+
+
+# Read the XLSX file
+df = pd.read_excel(PRE_TRAIN_FILE_PATH)
+
+# DEFINE CONDITIONS
+beforeOrAfter = (df['Iteration'] < 5567) | (df['Iteration'] > 8649)
+
+
+# Update the columns for every row
+df['X_Vel'] = X_VELOCITY 
+df['Z_Vel'] = Z_VELOCITY
+
+# Update the columns based on the conditions
+df.loc[beforeOrAfter, 'X_Vel'] = 0.000
+df.loc[beforeOrAfter, 'X_Vel'] = 0.000
+
+
+
+
+# Save the modified DataFrame to a new XLSX file
+df.to_excel(PROCESS_FILE_PATH, index=False)
