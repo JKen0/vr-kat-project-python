5b53c1927bc2baa66da0c552efd97bf74a7b5892
Ken <imken0@outlook.com>
2023-06-16 10:24:41 -0400

    data from unity will contain the history of sensor data, rather than calculated in python, updated script to just take that data and resize it to the shape of the requested neural network

--- python-servers/webSocketServer_Sync.py
+++ python-servers/webSocketServer_Sync.py
@@ -10,39 +10,18 @@ sys.path.append('C:/Dev/vr-kat-project-python-2/NeuralNetwork/')
 
 from ModelPredictor import ModelPredictor
 
-
+# IMPORTANT PARAMETERS FOR THE MODEL
 MAX_TIMESTEPS = 10
 NUMBER_OF_FEATURES = 4
 MODEL_PATH = 'test-model.h5'
 
-def update_previous_sensor_data(new_sensor_data, previous_sensor_data):
-
-    # Append the new sensor data to the list
-    previous_sensor_data.append(new_sensor_data)
-
         
-
-
-def process_request(model_predictor, message, previous_sensor_data):
+# THIS WILL  HANDLE THE MESSAGE RETURNED BY THE WEBSOCKET (AKA. THE GAME)
+def process_request(model_predictor, message):
     # Process the request and return the response
     sensor_data = json.loads(message)
 
-    L_Pitch_Data = sensor_data["L_Pitch"]
-    L_Roll_Data = sensor_data["L_Roll"]
-    R_Pitch_Data = sensor_data["R_Pitch"]
-    R_Roll_Data = sensor_data["R_Roll"]
-
-    # FEED THE SENSORS TO OUR NEURAL NETWORK
-    new_sensor_data = np.array([L_Pitch_Data, L_Roll_Data, R_Pitch_Data, R_Roll_Data])
-
-    #print(new_sensor_data)
-
-    # Append the new sensor data to the list
-    previous_sensor_data.append(new_sensor_data)
-
-    # Keep only the most recent 'max_timesteps' entries
-    if len(previous_sensor_data) >= MAX_TIMESTEPS:
-        previous_sensor_data = previous_sensor_data[-MAX_TIMESTEPS:]
+    input_data_reshape = np.reshape(np.array(sensor_data), (1, MAX_TIMESTEPS, NUMBER_OF_FEATURES))
 
     # IN RETURN WE WILL GET A VECT REPRESENTING VELOCITY
     response = {
@@ -51,46 +30,36 @@ def process_request(model_predictor, message, previous_sensor_data):
         'z_velocity': 0.000
     }
 
-    if(len(previous_sensor_data) == MAX_TIMESTEPS):
-        reshape_input_data = np.reshape(previous_sensor_data, (1, MAX_TIMESTEPS, NUMBER_OF_FEATURES))
-        reshape_input_data = reshape_input_data[:][::-1]
-
-        
-
-        #print(reshape_input_data)
-
-
-        velocity_prediction = model_predictor.predict(reshape_input_data)
-        response['x_velocity'] = str(round(velocity_prediction[0][0], 3))
-        response['z_velocity'] = str(round(velocity_prediction[0][1], 3))
+    velocity_prediction = model_predictor.predict(input_data_reshape)
+    response['x_velocity'] = str(round(velocity_prediction[0][0], 3))
+    response['z_velocity'] = str(round(velocity_prediction[0][1], 3))
 
     return response
 
+
+
+# CREATE WEBSOCKET SERVER ON PORT 3003
+# THIS SERVER IS HOW OUR GAME WILL COMMUNICATE TO OUR PYTHON SCRIPTs
 async def handle_websocket(websocket, path):
-    PREVIOUS_SENSOR_DATA = []
-    ITERATION = 0
-    
     model_predictor = ModelPredictor(MODEL_PATH)
     print('Socket is now using model: ', MODEL_PATH)
-
     
     print(str(datetime.now()) + ": Client connected")
     await websocket.send("You are now connected via WebSocket")
 
     try:
         while True:
-            #print(len(PREVIOUS_SENSOR_DATA))
-
             # RECIEVE MESSAGE FROM CLIENT
             message = await websocket.recv()
 
-
             # PROCESS AND CREATE RESPONSE FOR CLIENT
-            response = process_request(model_predictor, message, PREVIOUS_SENSOR_DATA)
+            response = process_request(model_predictor, message)
             response_json = json.dumps(response)
 
             # SEND RESPONSE TO CLIENT
             await websocket.send(response_json)
+
+
     except websockets.exceptions.ConnectionClosed:
         print(str(datetime.now())  + ": Client disconnected")
 
